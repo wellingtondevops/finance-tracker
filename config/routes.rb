df573bd5983e977fb7a381ac541c09992295dfc9
wellingtodevops <wellingtondevops@gmail.com>
2018-09-07 21:08:37 +0000

    Update views and navigations

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
     
-  devise_for :users
   
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
+  resources :user_stocks, only: [:create, :destroy]
   root 'welcome#index'
   get 'my_portfolio', to:"users#my_portfolio"
   #get 'search_stocks', to:'stocks#search'
   get 'search_stocks', to: 'stocks#search'
-  resources :user_stocks, only: [:create, :destroy]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_01_191628) do
+ActiveRecord::Schema.define(version: 2018_09_07_200156) do
 
   create_table "stocks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ticker"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2018_09_01_191628) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
