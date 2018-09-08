ada65b304fca4e06d8b0f1f2b44ebce6c76ab67b
wellingtodevops <wellingtondevops@gmail.com>
2018-09-08 01:51:47 +0000

    git add Friendship Model

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, only: [:create, :destroy]
+  
   root 'welcome#index'
   get 'my_portfolio', to:"users#my_portfolio"
   #get 'search_stocks', to:'stocks#search'
   get 'search_stocks', to: 'stocks#search'
-  
+  get 'my_friends', to: 'users#my_friends'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_200156) do
+ActiveRecord::Schema.define(version: 2018_09_08_014557) do
+
+  create_table "friendships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "stocks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ticker"
